b3a4871a3cc996c8a6ef9dd00ab82b83072bc172
Edson Cabral <edson.cabral@trescon.com.br>
2020-10-09 20:18:54 -0300

    🚧 add a static function to return the url

--- web/src/app/quotation-currency/services/currency.service.ts
+++ web/src/app/quotation-currency/services/currency.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import {HttpClient} from "@angular/common/http";
 import {Currency} from "../models/currency.model";
+import {environment} from "../../../environments/environment";
 
 @Injectable({
   providedIn: 'root'
@@ -9,9 +10,15 @@ export class CurrencyService {
 
   constructor(private http: HttpClient) { }
 
+  private static url(endPoint: string): string {
+    return `${environment.bcb_api_url}/${endPoint}`;
+  }
+
   async getCurrencies(): Promise<Array<Currency>> {
     try {
-      return this.http.get<Array<Currency>>('').toPromise();
+      return this.http
+        .get<Array<Currency>>(CurrencyService.url('Moedas?format=json'))
+        .toPromise();
     } catch (e) {
 
     }
