691fe7d3927bcc7c22d4dd1db12459997b2f8042
Edson Cabral <edson.cabral@trescon.com.br>
2020-10-09 20:35:56 -0300

    🚧 add a constructor to currency model

--- web/src/app/quotation-currency/models/currency.model.ts
+++ web/src/app/quotation-currency/models/currency.model.ts
@@ -1,6 +1,12 @@
 import {CurrencyType} from "./enum/currency-type.enum";
 
 export class Currency {
+  constructor(symbol: string, name: string, currencyType: CurrencyType) {
+    this.symbol = symbol;
+    this.name = name;
+    this.currencyType = currencyType;
+  }
+
   symbol: string;
   name: string;
   currencyType: CurrencyType;
