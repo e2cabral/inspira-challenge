2afde911ac8894c3bf1d729dba25f6583ab969f1
Edson Cabral <edson.cabral@trescon.com.br>
2020-10-09 20:20:28 -0300

    🚧 add a after view init life cycle hook

--- web/src/app/quotation-currency/quotation-currency.component.ts
+++ web/src/app/quotation-currency/quotation-currency.component.ts
@@ -1,15 +1,19 @@
-import { Component, OnInit } from '@angular/core';
+import {AfterViewInit, Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'quotation-currency',
   templateUrl: './quotation-currency.component.html',
   styleUrls: ['./quotation-currency.component.css']
 })
-export class QuotationCurrencyComponent implements OnInit {
+export class QuotationCurrencyComponent implements OnInit, AfterViewInit {
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  ngAfterViewInit() {
+
   }
 
 }
