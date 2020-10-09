2812e6106f3dcb7fd66cc015224f7c98c6168b71
Edson Cabral <edson.cabral@trescon.com.br>
2020-10-09 19:12:24 -0300

    💄 remove prefix app from component name

--- web/src/app/cotacao-moeda/components/cambio-moeda/cambio-moeda.component.ts
+++ web/src/app/cotacao-moeda/components/cambio-moeda/cambio-moeda.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-cambio-moeda',
+  selector: 'cambio-moeda',
   templateUrl: './cambio-moeda.component.html',
   styleUrls: ['./cambio-moeda.component.css']
 })
