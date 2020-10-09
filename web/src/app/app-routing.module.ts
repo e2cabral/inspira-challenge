ed19f4c64ca26dc268a9ca0fc8918be6370baa31
Edson Cabral <edson.cabral@trescon.com.br>
2020-10-09 18:49:41 -0300

    🚧 add a route to cotacao module

--- web/src/app/app-routing.module.ts
+++ web/src/app/app-routing.module.ts
@@ -2,7 +2,12 @@ import { NgModule } from '@angular/core';
 import {Routes, RouterModule, PreloadAllModules} from '@angular/router';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    loadChildren: () => import('./cotacao-moeda/cotacao-moeda.module').then(m => m.CotacaoMoedaModule)
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes, {
