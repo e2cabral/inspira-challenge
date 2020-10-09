e4b9cc9f125d3edac011c54af8f8d149f875d67a
Edson Cabral <edson.cabral@trescon.com.br>
2020-10-09 20:09:32 -0300

    🚧 add a method to get currencies

--- web/src/app/quotation-currency/services/currency.service.ts
+++ web/src/app/quotation-currency/services/currency.service.ts
@@ -1,9 +1,19 @@
 import { Injectable } from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import {Currency} from "../models/currency.model";
 
 @Injectable({
   providedIn: 'root'
 })
 export class CurrencyService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  async getCurrencies(): Promise<Array<Currency>> {
+    try {
+      return this.http.get<Array<Currency>>('').toPromise();
+    } catch (e) {
+
+    }
+  }
 }
