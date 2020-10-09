b287fb6053afe474635dbdc5154264986527ed04
Edson Cabral <edson.cabral@trescon.com.br>
2020-10-09 20:38:18 -0300

    💩 add a call to the 'Banco Central do Brasil' api

--- web/src/app/quotation-currency/quotation-currency.component.ts
+++ web/src/app/quotation-currency/quotation-currency.component.ts
@@ -1,5 +1,6 @@
 import {AfterViewInit, Component, OnInit} from '@angular/core';
 import {CurrencyService} from "./services/currency.service";
+import {Currency} from "./models/currency.model";
 
 @Component({
   selector: 'quotation-currency',
@@ -8,13 +9,17 @@ import {CurrencyService} from "./services/currency.service";
 })
 export class QuotationCurrencyComponent implements OnInit, AfterViewInit {
 
+  currencies: Array<Currency>;
+
   constructor(private currencyService: CurrencyService) { }
 
   ngOnInit() {
   }
 
   ngAfterViewInit() {
-
+    this.currencyService
+      .getCurrencies()
+      .then(({ values }) => this.currencies = values.map(({ symbol, name, currencyType }) => new Currency(symbol, name, currencyType)));
   }
 
 }
