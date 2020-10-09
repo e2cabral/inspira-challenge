adb20b1ff2df303718e26a4e2c159816b434c50f
Edson Cabral <edson.cabral@trescon.com.br>
2020-10-09 18:49:03 -0300

    🚧 import cotacao component and module in the app module

--- web/src/app/app.module.ts
+++ web/src/app/app.module.ts
@@ -4,6 +4,8 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { CotacaoMoedaModule } from "./cotacao-moeda/cotacao-moeda.module";
+
 @NgModule({
   declarations: [
     AppComponent
@@ -11,6 +13,8 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
+
+    CotacaoMoedaModule
   ],
   providers: [],
   bootstrap: [AppComponent]
