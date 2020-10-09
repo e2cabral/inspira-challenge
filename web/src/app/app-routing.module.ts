cbed41ba290a1989332358590a7dfe834f4cde1b
Edson Cabral <edson.cabral@trescon.com.br>
2020-10-09 19:23:48 -0300

    ♻️ change imports to match the new name

--- web/src/app/app-routing.module.ts
+++ web/src/app/app-routing.module.ts
@@ -5,7 +5,7 @@ import {Routes, RouterModule, PreloadAllModules} from '@angular/router';
 const routes: Routes = [
   {
     path: '',
-    loadChildren: () => import('./cotacao-moeda/cotacao-moeda.module').then(m => m.CotacaoMoedaModule)
+    loadChildren: () => import('./cotacao-moeda/quotation-currency.module').then(m => m.QuotationCurrencyModule)
   }
 ];
 
--- web/src/app/app.module.ts
+++ web/src/app/app.module.ts
@@ -4,7 +4,7 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
-import { CotacaoMoedaModule } from "./cotacao-moeda/cotacao-moeda.module";
+import { QuotationCurrencyModule } from "./cotacao-moeda/quotation-currency.module";
 
 @NgModule({
   declarations: [
@@ -14,7 +14,7 @@ import { CotacaoMoedaModule } from "./cotacao-moeda/cotacao-moeda.module";
     BrowserModule,
     AppRoutingModule,
 
-    CotacaoMoedaModule
+    QuotationCurrencyModule
   ],
   providers: [],
   bootstrap: [AppComponent]
