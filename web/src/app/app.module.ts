36edd57525467d09e86c469ff7c1cd400f18931b
Edson Cabral <edson.cabral@trescon.com.br>
2020-10-09 20:35:20 -0300

    🚧 import http client module

--- web/src/app/app.module.ts
+++ web/src/app/app.module.ts
@@ -1,4 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from "@angular/common/http";
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
@@ -13,6 +14,7 @@ import { QuotationCurrencyModule } from "./quotation-currency/quotation-currency
   imports: [
     BrowserModule,
     AppRoutingModule,
+    HttpClientModule,
 
     QuotationCurrencyModule
   ],
