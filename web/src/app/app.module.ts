a5de05ba387b328edd1bf9b30e19c5e6bca460af
Edson Cabral <edson.cabral@trescon.com.br>
2020-10-10 11:26:38 -0300

    🚧 add imports new modules

--- web/src/app/app.module.ts
+++ web/src/app/app.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from "@angular/common/http";
 import { ReactiveFormsModule } from "@angular/forms";
+import { NgxSpinnerModule } from "ngx-spinner";
 
 import { QuotationCurrencyModule } from "./quotation-currency/quotation-currency.module";
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {CoreModule} from "./core/core.module";
 
 @NgModule({
   declarations: [
@@ -17,6 +19,8 @@ import { AppComponent } from './app.component';
     AppRoutingModule,
     HttpClientModule,
     ReactiveFormsModule,
+    CoreModule,
+    NgxSpinnerModule,
 
     QuotationCurrencyModule
   ],
