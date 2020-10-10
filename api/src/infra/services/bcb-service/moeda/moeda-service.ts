16deb08ce7910f3b05a4be97d4e6491edef08d73
Edson Cabral <e2cabraljunior@gmail.com>
2020-10-10 17:04:30 -0300

    🚧 implements obter moeda service interface

--- api/src/infra/services/bcb-service/moeda/moeda-service.ts
+++ api/src/infra/services/bcb-service/moeda/moeda-service.ts
@@ -1,3 +1,9 @@
-export class MoedaService {
-  
+import {IObterMoedasService} from "../../../../data/services/moeda/i-obter-moedas-service";
+import {Moeda} from "../../../../domain/model/moeda-model";
+
+export class MoedaService implements IObterMoedasService {
+  async obter(): Promise<Array<Moeda>> {
+    return [];
+  }
+
 }
