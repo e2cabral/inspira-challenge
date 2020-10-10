f2b82db24c0d7bf160de0aca94c772554a921556
Edson Cabral <edson.cabral@trescon.com.br>
2020-10-09 21:12:40 -0300

    🚧 add an input currency property

--- web/src/app/quotation-currency/components/currency/currency.component.ts
+++ web/src/app/quotation-currency/components/currency/currency.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
+import {Currency} from "../../models/currency.model";
 
 @Component({
   selector: 'cambio-moeda',
@@ -6,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./currency.component.css']
 })
 export class CurrencyComponent implements OnInit {
+
+  @Input() currencies: Array<Currency>;
 
   constructor() { }
 
