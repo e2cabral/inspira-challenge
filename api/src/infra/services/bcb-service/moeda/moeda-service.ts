9198b267f9a9393a3ce68c4423afc369d0265855
Edson Cabral <e2cabraljunior@gmail.com>
2020-10-10 17:45:59 -0300

    💩 add a request to the bcb api

--- api/src/infra/services/bcb-service/moeda/moeda-service.ts
+++ api/src/infra/services/bcb-service/moeda/moeda-service.ts
@@ -1,9 +1,13 @@
 import {IObterMoedasService} from "../../../../data/services/moeda/i-obter-moedas-service";
-import {Moeda} from "../../../../domain/model/moeda-model";
+import env from '../../../../main/config/env';
+import https from 'https';
 
 export class MoedaService implements IObterMoedasService {
-  async obter(): Promise<Array<Moeda>> {
-    return [];
+  async obter(): Promise<void> {
+    await https.get(
+      `${env.bcb_api_url}/Moedas?format=json`,
+      { headers: { contentType: 'application/json' } }, (response) => {
+      response.on('data', (chunk) => JSON.parse(chunk.value));
+    }).end();
   }
-
 }
