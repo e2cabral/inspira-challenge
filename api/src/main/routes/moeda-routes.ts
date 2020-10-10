f57869c36bda21cd485ced73f4e5a41b000e851d
Edson Cabral <e2cabraljunior@gmail.com>
2020-10-10 17:45:07 -0300

    🐛 fix the endpoint

--- api/src/main/routes/moeda-routes.ts
+++ api/src/main/routes/moeda-routes.ts
@@ -2,5 +2,5 @@ import {Router} from "express";
 import MoedaController from '../../presentation/controllers/moeda/moeda-controller'
 
 export default (router: Router): void => {
-  router.get('/Moedas?$format=json', MoedaController.obter);
+  router.get('/moedas', MoedaController.obter.bind(MoedaController));
 }
