a617869c38792a1c4802d85e67356dde558ff750
Edson Cabral <e2cabraljunior@gmail.com>
2020-10-10 16:40:23 -0300

    🚧 implements the interface obter moedas

--- api/src/presentation/controllers/moeda/moeda-controller.ts
+++ api/src/presentation/controllers/moeda/moeda-controller.ts
@@ -1,4 +1,12 @@
-class MoedaController {
+import {IObterMoedas} from "../../../domain/usecases/moeda/i-obter-moedas";
+import {Moeda} from "../../../domain/model/moeda-model";
+import {Request, Response} from "express";
+
+class MoedaController implements IObterMoedas {
+
+  async obter(req: Request, res: Response): Promise<Response<Array<Moeda>>> {
+    return res.json([]);
+  }
 
 }
 
