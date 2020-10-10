8ba154a59c5c42fafa62263b9ba057ce4b04ca94
Edson Cabral <e2cabraljunior@gmail.com>
2020-10-10 20:01:31 -0300

    🚧 add a new route to the environment variables

--- web/src/environments/environment.ts
+++ web/src/environments/environment.ts
@@ -4,7 +4,8 @@
 
 export const environment = {
   production: false,
-  bcb_api_url: 'https://olinda.bcb.gov.br/olinda/servico/PTAX/versao/v1/odata'
+  bcb_api_url: 'https://olinda.bcb.gov.br/olinda/servico/PTAX/versao/v1/odata',
+  bcb_api_interceptor: 'http://localhost:3000/v1/api'
 };
 
 /*
