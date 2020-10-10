fca9f9a0cadecb49429ba58fa0786bdfe5c4d945
Edson Cabral <e2cabraljunior@gmail.com>
2020-10-10 16:47:44 -0300

    🚧 add a controller to moeda routes

--- api/src/main/routes/moeda-routes.ts
+++ api/src/main/routes/moeda-routes.ts
@@ -1,5 +1,6 @@
 import {Router} from "express";
+import MoedaController from '../../presentation/controllers/moeda/moeda-controller'
 
 export default (router: Router): void => {
-  router.get('/Moedas?$format=json');
+  router.get('/Moedas?$format=json', MoedaController.obter);
 }
