a562d3606f58bd0d7f4f28028dca3cd1183357e2
Edson Cabral <e2cabraljunior@gmail.com>
2020-10-10 19:04:36 -0300

    🚧 add https method to call bcb api

--- api/src/presentation/controllers/moeda/moeda-controller.ts
+++ api/src/presentation/controllers/moeda/moeda-controller.ts
@@ -1,12 +1,22 @@
 import {IObterMoedas} from "../../../domain/usecases/moeda/i-obter-moedas";
-import {Moeda} from "../../../domain/model/moeda-model";
 import {Request, Response} from "express";
 import {MoedaService} from "../../../infra/services/bcb-service/moeda/moeda-service";
 
+import https from 'https'
+import env from "../../../main/config/env";
+
 class MoedaController implements IObterMoedas {
   constructor(private readonly moedaService: MoedaService) {}
-  async obter(req: Request, res: Response): Promise<Response<Array<Moeda>>> {
-    return res.json(this.moedaService.obter());
+
+  async obter(req: Request, res: Response) {
+    try {
+      await https.get(
+        `${env.bcb_api_url}/Moedas?format=json`,
+        (response) => this.moedaService.obter(response, res)
+      );
+    } catch (error) {
+      console.log(error);
+    }
   }
 
 }
