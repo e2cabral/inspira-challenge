2d156c050d11d94d36b2dbd2df1327fb32498bf9
Edson Cabral <e2cabraljunior@gmail.com>
2020-10-10 19:00:39 -0300

    ♻️ change response type in obter moedas interface

--- api/src/domain/usecases/moeda/i-obter-moedas.ts
+++ api/src/domain/usecases/moeda/i-obter-moedas.ts
@@ -1,6 +1,5 @@
 import {Request, Response} from "express";
-import {Moeda} from "../../model/moeda-model";
 
 export interface IObterMoedas {
-  obter: (req: Request, res: Response) => Promise<Response<Array<Moeda>>>;
+  obter: (req: Request, res: Response) => Promise<void>;
 }
