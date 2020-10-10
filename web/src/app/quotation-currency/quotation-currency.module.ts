48d92ee68bdc2ddc5c5b76a0aa05b3239d886e60
Edson Cabral <edson.cabral@trescon.com.br>
2020-10-09 21:44:30 -0300

    🚧 add ng select config to providers

--- web/src/app/quotation-currency/quotation-currency.module.ts
+++ web/src/app/quotation-currency/quotation-currency.module.ts
@@ -3,9 +3,12 @@ import { CommonModule } from '@angular/common';
 import { NgSelectModule } from "@ng-select/ng-select";
 
 import {QuotationCurrencyRoutingModule} from "./quotation-currency-routing.module";
+import {ReactiveFormsModule} from "@angular/forms";
 
 import {QuotationCurrencyComponent} from "./quotation-currency.component";
 import { CurrencyComponent } from './components/currency/currency.component';
+import { NgSelectConfig } from '@ng-select/ng-select';
+import { ɵs } from '@ng-select/ng-select';
 
 
 
@@ -18,7 +21,12 @@ import { CurrencyComponent } from './components/currency/currency.component';
     CommonModule,
     NgSelectModule,
 
-    QuotationCurrencyRoutingModule
+    QuotationCurrencyRoutingModule,
+    ReactiveFormsModule
+  ],
+  providers: [
+    NgSelectConfig,
+    ɵs
   ]
 })
 export class QuotationCurrencyModule { }
