3f9fc498a7e35248764394fb38148889859ed77c
Edson Cabral <e2cabraljunior@gmail.com>
2020-10-10 17:43:57 -0300

    🚧 add moeda service to moeda controller

--- api/src/presentation/controllers/moeda/moeda-controller.ts
+++ api/src/presentation/controllers/moeda/moeda-controller.ts
@@ -1,13 +1,14 @@
 import {IObterMoedas} from "../../../domain/usecases/moeda/i-obter-moedas";
 import {Moeda} from "../../../domain/model/moeda-model";
 import {Request, Response} from "express";
+import {MoedaService} from "../../../infra/services/bcb-service/moeda/moeda-service";
 
 class MoedaController implements IObterMoedas {
-
+  constructor(private readonly moedaService: MoedaService) {}
   async obter(req: Request, res: Response): Promise<Response<Array<Moeda>>> {
-    return res.json([]);
+    return res.json(this.moedaService.obter());
   }
 
 }
 
-export default new MoedaController();
+export default new MoedaController(new MoedaService());
