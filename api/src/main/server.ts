c3bc963f3fc779519923ff37db9e96b3eeebff27
Edson Cabral <e2cabraljunior@gmail.com>
2020-10-11 16:09:31 -0300

    🚧 change the default port

--- api/src/main/server.ts
+++ api/src/main/server.ts
@@ -1,3 +1,3 @@
 import app from './config/app';
 
-app.listen(process.env.PORT || 3000, () => console.log('A API do projeto Inspira está rodando.'));
+app.listen(process.env.PORT || 8080, () => console.log('A API do projeto Inspira está rodando.'));
