981d36ed5a95322f45515dfb6dce95fac2c84f97
Edson Cabral <e2cabraljunior@gmail.com>
2020-10-11 10:56:06 -0300

    🚧 change the default url to the api

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   bcb_api_url: 'https://olinda.bcb.gov.br/olinda/servico/PTAX/versao/v1/odata',
-  bcb_api_interceptor: 'http://localhost:3000/v1/api'
+  bcb_api_interceptor: `https://${window.location.host}/v1/api`
 };
